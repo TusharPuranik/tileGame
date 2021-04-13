dddceae025e9ec06e2e6fed8cb7414c2c108859f
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-13 23:50:34 +0530

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -1,27 +1,22 @@
 package tilegame.states;
 import java.awt.Graphics;
+import tilegame.entities.creatures.Player;
 import tilegame.gfx.Assets;
-
 
 public class GameState extends State{
     
+    private Player player;
     public GameState()
     {
-        
+        player=new Player(10,100);
     }
-
     @Override
     public void update() {
-        
+        player.update();
         
     }
-
     @Override
     public void render(Graphics g) {
-        
-        g.drawImage(Assets.dirt, 0, 0, null);
+        player.render(g);
     }
-    
-    
-    
 }
