6370cf461776886d133a6c792c1695986dc1c483
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-13 12:17:17 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -19,15 +19,12 @@ public class Game implements Runnable{
     private Thread thread;
     
     private Graphics g;
-
     private BufferStrategy bs;
     
     /*buffer is a hidden screen in your computer that you cannot see. In other words its a hidden memeory which
     contains the same data as the actual screen does*/
-    
-    /*buffer strategy helps the pc to understand how to draw elements ot the screen*/
-    
-    
+
+    /*buffer strategy helps the pc to understand how to draw elements ot the screen*/    
     public Game(String title,int width,int height)
     {
         this.title=title;
@@ -51,26 +48,51 @@ public class Game implements Runnable{
         g.clearRect(0, 0, width, height);
         //drawing starts from here
         
-            
-            
-
-        
         //drawing ends here
         bs.show();
         g.dispose();
     }
     
-    
     public void run()
     {
         init();
+       /*game loop timer is used so that the movements of all the elements in the 
+        panel move equally in all computers and do not fluctuate
+        */
+        
+        //game loop timer start here
+        int fps=60;
+        double timePerUpdate=1000000000/fps;
+        double delta=0;
+        long now;
+        long lastTime=System.nanoTime();//System.nanoTime() returns the time that the computer is running at in nanao seconds.
+        long timer=0;
+        int ticks=0;
+        
         while(running)
         {
-            update();
-            render();
+            now=System.nanoTime();
+            delta+=(now-lastTime)/timePerUpdate;//delta will store the time remaining for the computer call update() and render() methods.
+            //delta tells the computer when and when not to call update() and render().
+            timer+=now-lastTime;
+            lastTime=now;
+                    
+            if(delta>=1)
+            {
+                update();
+                render();
+                ticks++;
+                delta--;
+            }
+            if(timer>=1000000000)
+            {
+                System.out.println("ticks"+ticks);
+                ticks=0;
+                timer=0;
+            }
+            //game loop timer ends here
         }
         stop();
-        
     }
     
     public void init()//initialize
@@ -86,7 +108,6 @@ public class Game implements Runnable{
         running = true;
        thread=new Thread(this);
        thread.start();
-       
     }
     public synchronized void stop()
     {
@@ -99,5 +120,4 @@ public class Game implements Runnable{
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
 }
