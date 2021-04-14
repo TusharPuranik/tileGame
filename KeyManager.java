7a3f2002019cf3296c998f3959fd2f2342ad5530
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-14 23:06:15 +0530

    Update KeyManager.java

--- KeyManager.java
+++ KeyManager.java
@@ -14,17 +14,15 @@ public class KeyManager implements KeyListener {
     }
     public void update()
     {
-        up=keys[KeyEvent.VK_UP];
-        down=keys[KeyEvent.VK_DOWN];
-        left=keys[KeyEvent.VK_LEFT];
-        right=keys[KeyEvent.VK_RIGHT];
+        up=keys[KeyEvent.VK_W];
+        down=keys[KeyEvent.VK_S];
+        left=keys[KeyEvent.VK_A];
+        right=keys[KeyEvent.VK_D];
     }
       
     @Override
     public void keyPressed(KeyEvent e) {
-        
         keys[e.getKeyCode()]=true;
-         System.out.println("Pressed!");
     }
     @Override
     public void keyReleased(KeyEvent e) {
