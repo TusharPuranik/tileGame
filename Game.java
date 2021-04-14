9f629a803270898f2c204a9aa1e598e7c86ad78a
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-14 11:40:34 +0530

    Update Game.java
    Methods required for KeyManager class are added.

--- Game.java
+++ Game.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import tilegame.gfx.Assets;
 import tilegame.gfx.ImageLoader;
 import tilegame.gfx.SpriteSheets;
+import tilegame.input.KeyManager;
 import tilegame.states.GameState;
 import tilegame.states.MenuState;
 import tilegame.states.SettingState;
@@ -23,28 +24,37 @@ public class Game implements Runnable{
     private Thread thread;
     
     private Graphics g;
-
     private BufferStrategy bs;
     
     /*buffer is a hidden screen in your computer that you cannot see. In other words its a hidden memeory which
     contains the same data as the actual screen does*/
-    
     /*buffer strategy helps the pc to understand how to draw elements ot the screen*/
     
-
     //States
     private State gameState;
     private State menuState;
     private State settingState;
+    
+    private KeyManager keyManager;
     
     public Game(String title,int width,int height)
     {
         this.title=title;
         this.width=width;
         this.height=height;
+        keyManager=new KeyManager();
     }
-    
-    
+    public void init()//initialize
+    {
+         display=new Display(title,width,height);
+         display.getCanvas().addKeyListener(keyManager);
+         Assets.init();
+         
+         gameState=new GameState(this);
+         menuState=new MenuState(this);
+         settingState=new SettingState(this);
+         State.setState(gameState);
+    }
     public void update()//keeps in updating the game
     {
       if(State.getState()!=null)
@@ -60,27 +70,21 @@ public class Game implements Runnable{
         }
         g=bs.getDrawGraphics();
         g.clearRect(0, 0, width, height);
-        //drawing starts from here
-        
+        //drawing starts from here        
        
         if(State.getState()!=null)
           State.getState().render(g);
 
-        
         //drawing ends here
         bs.show();
         g.dispose();
     }
-    
-    
     public void run()
     {
         init();
-        
         /*game loop timer is used so that the movements of all the elements in the 
         panel move equally in all computers and do not fluctuate
         */
-        
         //game loop timer start here
         int fps=60;
         double timePerUpdate=1000000000/fps;
@@ -89,16 +93,13 @@ public class Game implements Runnable{
         long lastTime=System.nanoTime();//System.nanoTime() returns the time that the computer is running at in nanao seconds.
         long timer=0;
         int ticks=0;
-        
         while(running)
         {
             now=System.nanoTime();
             delta+=(now-lastTime)/timePerUpdate;//delta will store the time remaining for the computer call update() and render() methods.
             //delta tells the computer when and when not to call update() and render().
             timer+=now-lastTime;
-            lastTime=now;
-            
-            
+            lastTime=now;            
             if(delta>=1)
             {
                 update();
@@ -108,26 +109,13 @@ public class Game implements Runnable{
             }
             if(timer>=1000000000)
             {
-                System.out.println("ticks"+ticks);
-                ticks=0;
+                System.out.println("ticks:60");
                 timer=0;
+                ticks=0;
             }
             //game loop timer ends here
         }
-        stop();
-        
-    }
-    
-    public void init()//initialize
-    {
-         display=new Display(title,width,height);
-         Assets.init();
-         
-         gameState=new GameState();
-         menuState=new MenuState();
-         settingState=new SettingState();
-        
-         State.setState(gameState);
+        stop();   
     }
     public synchronized void start()
     {
@@ -150,5 +138,5 @@ public class Game implements Runnable{
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
 }
+
