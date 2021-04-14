6c7e081e9fb18b143f3a46e540d2d7eafacf2c57
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-14 11:41:57 +0530

    Update Display.java
    Added getter method for JFrame

--- Display.java
+++ Display.java
@@ -36,6 +36,7 @@ public class Display {
         canvas.setPreferredSize(new Dimension(width,height));
         canvas.setMaximumSize(new Dimension(width,height));
         canvas.setMinimumSize(new Dimension(width,height));
+        canvas,setFocusable(false);
         frame.add(canvas);
         frame.pack();
         
@@ -44,6 +45,8 @@ public class Display {
     {
         return canvas;
     }
-    
-    
+    public JFrame getFrame()
+    {
+        return frame;
+    }
 }
