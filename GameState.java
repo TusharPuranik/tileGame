ef52831677d04545863485d3dfab4ed4372dbc61
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-17 01:45:44 +0530

    Update GameState.java

--- GameState.java
+++ GameState.java
@@ -2,25 +2,30 @@ package tilegame.states;
 import java.awt.Graphics;
 import tilegame.Game;
 import tilegame.entities.creatures.Player;
-import tilegame.gfx.Assets;
+import tilegame.worlds.World;
 
 public class GameState extends State{
     
     private Player player;
-    
+    private World world;
+            
     public GameState(Game game)
     {
         super(game);
         player=new Player(game,10,100);
+        world=new World("");
     }
 
     @Override
     public void update() {
+        world.update();
         player.update();
     }
 
     @Override
     public void render(Graphics g) {
+        world.render(g);
         player.render(g);
+       
     }
 }
