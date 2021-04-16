2b32e9431a562e467838f0bf2240b88ef9ffd1fc
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-17 01:21:33 +0530

    Add files via upload
    Tiles classes are added which will help in creating the worlds or maps required in this game.

---
+++ DirtTile.java
@@ -0,0 +1,9 @@
+package tilegame.tile;
+import tilegame.gfx.Assets;
+
+public class DirtTile extends Tile{
+
+    public DirtTile(int id) {
+        super(Assets.dirt, id);
+    }
+}
---
+++ RockTile.java
@@ -0,0 +1,15 @@
+package tilegame.tile;
+import tilegame.gfx.Assets;
+
+public class RockTile extends Tile{
+
+    public RockTile(int id) {
+        super(Assets.stone, id);
+    }
+    
+    @Override
+    public boolean isSolid()
+    {
+        return true;
+    }
+}
