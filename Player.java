ec4c0a94bc0bea175609dea723136c4bed6d6249
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-16 23:33:53 +0530

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,43 +1,43 @@
 package tilegame.entities.creatures;
-
 import java.awt.Graphics;
+import tilegame.Game;
 import tilegame.gfx.Assets;
-import tilegame.*;
 
 public class Player extends Creature
 {
     private Game game;
     public Player(Game game,float x,float y)
     {
-        super(x,y);
+        super(x,y,Creature.DEFAULT_CREATURE_WIDTH,Creature.DEFAULT_CREATURE_HEIGHT);
         this.game=game;
     }
 
     @Override
     public void update() {
-
-if(game.getKeyManager().up)
-        {
-            y-=3;
-        }
-        if(game.getKeyManager().down)
-        {
-            y+=3;
-        }
-        if(game.getKeyManager().left)
-        {
-            x-=3;
-        }
-        if(game.getKeyManager().right)
-        {
-            x+=3;
-        }
-
+        getInput();
+        move();
+               
     }
+    
+    private void getInput()
+    {
+        xMove=0;
+        yMove=0;
+        
+        if(game.getKeyManager().up)
+            yMove=-speed;
+        if(game.getKeyManager().left)
+            xMove=-speed;
+        if(game.getKeyManager().down)
+            yMove=speed;
+        if(game.getKeyManager().right)
+            xMove=speed;
+    }
+    
 
     @Override
-    public void render(Graphics g) {
-        
-        g.drawImage(Assets.player, (int)x, (int)y, null);   
+    public void render(Graphics g) {       
+        g.drawImage(Assets.player, (int)x, (int)y,width,height, null);
+    
     }
 }
