1164187e5715343c0eb75bf725a2b920f60c7af7
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-16 23:32:57 +0530

    Update Creature.java

--- Creature.java
+++ Creature.java
@@ -1,18 +1,61 @@
 package tilegame.entities.creatures;
-
 import java.awt.Graphics;
 import tilegame.entities.Entity;
 
-
 public abstract class Creature extends Entity {
 
+    public static final int DEFAULT_HEALTH=10;
+    public static final float DEFAULT_SPEED=3.0f;
+    public static final int DEFAULT_CREATURE_WIDTH=64;
+    public static final int DEFAULT_CREATURE_HEIGHT=64;
+
     protected int health;
+    protected float speed;// This is added as we want our every creature to move.
+    protected float xMove,yMove;
     
-    public Creature(float x, float y) {
-        super(x, y);
-        health=10;
+    public Creature(float x, float y,int width,int height) {
+        super(x, y,width,height);
+        health=DEFAULT_HEALTH;
+        speed=DEFAULT_SPEED;
+        xMove=0;
+        yMove=0;
     }
     
-   
-    
+    public void move()
+    {
+        x+=xMove;
+        y+=yMove;
+    }
+
+    public float getxMove() {
+        return xMove;
+    }
+
+    public void setxMove(float xMove) {
+        this.xMove = xMove;
+    }
+
+    public float getyMove() {
+        return yMove;
+    }
+
+    public void setyMove(float yMove) {
+        this.yMove = yMove;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }    
 }
