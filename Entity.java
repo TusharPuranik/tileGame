86cf8bf54eff315281be6050dd06af3f9a716b18
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-17 23:17:29 +0530

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,13 +1,16 @@
 package tilegame.entities;
 import java.awt.Graphics;
+import tilegame.Game;
 
 public abstract class Entity {
     
+    protected Game game;
     protected float x,y;
     protected int width,height;
     
-    public Entity(float x,float y,int width,int height)
+    public Entity(Game game,float x,float y,int width,int height)
     {
+        this.game=game;
         this.x=x;
         this.y=y;
         this.width=width;
