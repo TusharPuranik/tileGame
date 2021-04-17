d20c0f485b747a180230c81317ac677b591ec70a
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-18 00:48:44 +0530

    Add files via upload

---
+++ TreeTile.java
@@ -0,0 +1,12 @@
+package tilegame.tile;
+
+import tilegame.gfx.Assets;
+
+
+public class TreeTile extends Tile{
+    
+    public TreeTile(int id)
+    {
+        super(Assets.tree, id);
+    }    
+}
