9ee664450311fd44050f696cab5cef75ba99e784
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-18 00:48:21 +0530

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -8,6 +8,7 @@ public class Tile {
     public static Tile grassTile=new GrassTile(0);
     public static Tile dirtTile=new DirtTile(1);
     public static Tile rockTile=new RockTile(2);
+    public static Tile treeTile=new TreeTile(3);
 
     
     public static final int TILEWIDTH=64,TILEHEIGHT=64;
