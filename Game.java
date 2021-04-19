32fcd845f6580aa2ce3de790c41e4cb317f77543
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-19 22:37:14 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -53,9 +53,8 @@ public class Game implements Runnable{
          display.getframe().addKeyListener(keyManager);
          Assets.init();
          
-         gameCamera=new GameCamera(this , 0, 0);
-         
          handler=new Handler(this);
+         gameCamera=new GameCamera(handler , 0, 0);
          
          gameState=new GameState(handler);
          menuState=new MenuState(handler);
