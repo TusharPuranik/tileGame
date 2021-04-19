0ea25c393b3497a0c256a2c0055610862f491d55
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-20 00:14:12 +0530

    Update Player.java
    Animations are added.

--- Player.java
+++ Player.java
@@ -1,12 +1,13 @@
 package tilegame.entities.creatures;
-import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import tilegame.Handler;
+import tilegame.gfx.Animation;
 import tilegame.gfx.Assets;
 
 public class Player extends Creature
 {
-    
+    private Animation animDown,animUp,animLeft,animRight,animIdle;
     public Player(Handler handler,float x,float y)
     {
         super(handler, x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
@@ -14,10 +15,24 @@ public class Player extends Creature
         bounds.y=16;
         bounds.width=32;
         bounds.height=45;
+        
+        //Animations
+        animDown=new Animation(150,Assets.player_down);
+        animUp=new Animation(150,Assets.player_up);
+        animLeft=new Animation(150,Assets.player_left);
+        animRight=new Animation(150,Assets.player_right);
+        animIdle=new Animation(150,Assets.player_idle);
     }
 
     @Override
     public void update() {
+        //Animations
+        animDown.update();
+        animUp.update();
+        animLeft.update();
+        animRight.update();
+        animIdle.update();
+        
         getInput();
         move();
         handler.getGameCamera().centerOnEntity(this);
@@ -42,9 +57,32 @@ public class Player extends Creature
     @Override
     public void render(Graphics g) {
         
-        g.drawImage(Assets.player, (int)(x - handler.getGameCamera().getxOffset()), 
+        g.drawImage(getCurrentAnimationFrame(), (int)(x - handler.getGameCamera().getxOffset()), 
                                     (int)(y - handler.getGameCamera().getyOffset()), width, height, null);
-        
+    }
+    
+    private BufferedImage getCurrentAnimationFrame()
+    {
+        if(xMove<0)
+        {
+            return animLeft.getFrames();
+        }
+        else if(xMove>0)
+        {
+            return animRight.getFrames();
+        }
+        else if(yMove>0)
+        {
+            return animDown.getFrames();
+        }
+        else if(yMove<0)
+        {
+            return animUp.getFrames();
+        }
+        else
+        {
+            return animIdle.getFrames();
+        }
     }
     
 }
