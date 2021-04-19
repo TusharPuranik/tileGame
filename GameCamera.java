b09f399bf32306a614400788df702df06451ceb1
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-19 22:38:08 +0530

    Update GameCamera.java

--- GameCamera.java
+++ GameCamera.java
@@ -4,31 +4,55 @@ so that our player is not lost.
 
 package tilegame.gfx;
 import tilegame.Game;
+import tilegame.Handler;
 import tilegame.entities.Entity;
+import tilegame.tile.Tile;
 
 public class GameCamera {
 
-    private Game game;
+    private Handler handler;
     private float xOffset,yOffset;
     //offsets are used to render the position of the screen that need to be moved.
 
-    public GameCamera(Game game,float xOffset,float yOffset)
+    public GameCamera(Handler handler,float xOffset,float yOffset)
     {
         this.xOffset=xOffset;
         this.yOffset=yOffset;
-        this.game=game;
+        this.handler=handler;
     }
 
+    public void checkBlankSpace()// Camera limit code . This is to remove the blank space outside the map.
+    {
+        if(xOffset<0)
+        {
+            xOffset=0;
+        }
+        else if(xOffset>handler.getWorld().getWidth()*Tile.TILEWIDTH-handler.getWidth())
+        {
+            xOffset=handler.getWorld().getWidth()*Tile.TILEWIDTH-handler.getWidth();
+        }
+        if(yOffset<0)
+        {
+            yOffset=0;
+        }
+        else if(yOffset>handler.getWorld().getHeight()*Tile.TILEHEIGHT-handler.getHeight())
+        {
+            yOffset=handler.getWorld().getHeight()*Tile.TILEHEIGHT-handler.getHeight();
+        }
+    }
+    
     public void centerOnEntity(Entity e)
     {
-        xOffset= e.getX()- game.getWidth() / 2 + e.getWidth() / 2;
-        yOffset= e.getY()- game.getHeight() / 2 + e.getHeight() / 2;
+        xOffset= e.getX()- handler.getWidth() / 2 + e.getWidth() / 2;
+        yOffset= e.getY()- handler.getHeight() / 2 + e.getHeight() / 2;
+        checkBlankSpace();
     }
     
     public void move(float xAmt,float yAmt)
     {
         xOffset+=xAmt;
         yOffset+=yAmt;
+        checkBlankSpace();
     }
     
     public float getxOffset() {
