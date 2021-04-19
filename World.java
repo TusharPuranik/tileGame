0b57fa9d1977e90f9fca8b8fbcb4c85e4d486bbd
TusharPuranik <81562207+TusharPuranik@users.noreply.github.com>
2021-04-19 22:40:55 +0530

    Update World.java

--- World.java
+++ World.java
@@ -71,4 +71,12 @@ public class World {
             }
         }
     }
+    public int getWidth()
+    {
+        return width;
+    }
+    public int getHeight()
+    {
+        return height;
+    }
 }
